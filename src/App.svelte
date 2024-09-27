7efb399cdea6c149fc7faf4b1f5075758c183c59
Christian Griggs <griggsca91@gmail.com>
2024-09-27 07:01:25 -0400

    styling

--- src/App.svelte
+++ src/App.svelte
@@ -151,30 +151,35 @@
     }
 </script>
 
-<button onclick={addRequest}>Add Request</button>
+<div class="container m-2 p-2">
+    <button onclick={addRequest}>Add Request</button>
 
-<div class="container flex-row">
-    {#each requests.requests as r}
-        <!-- svelte-ignore a11y_click_events_have_key_events -->
-        <!-- svelte-ignore a11y_no_static_element_interactions -->
-        <div
-            class="flex flex-column"
-            onclick={(e) => {
-                selectedRequest = r;
-                openPanel = true;
-                e.stopPropagation();
-            }}
-        >
-            <div class="basis-1/4 flex-row">
-                <div class="basis-1/2">
-                    {r.url}
-                </div>
-                <div class="text-xs basis-1/2">
-                    {r.requestTime}
+    <div class="container flex-row space-y-4">
+        {#each requests.requests as r}
+            <!-- svelte-ignore a11y_click_events_have_key_events -->
+            <!-- svelte-ignore a11y_no_static_element_interactions -->
+            <div
+                class="flex flex-column"
+                onclick={(e) => {
+                    selectedRequest = r;
+                    openPanel = true;
+                    e.stopPropagation();
+                }}
+            >
+                <div class="basis-1/4 flex-row">
+                    <div class="basis-1/2">
+                        {r.url}
+                    </div>
+                    <div class="text-xs basis-1/2">
+                        {r.requestTime}
+                    </div>
                 </div>
             </div>
-        </div>
-    {/each}
+        {/each}
+    </div>
 </div>
-
-<Panel closePanel={() => openPanel = false} request={selectedRequest} isOpen={openPanel} />
+<Panel
+    closePanel={() => (openPanel = false)}
+    request={selectedRequest}
+    isOpen={openPanel}
+/>
